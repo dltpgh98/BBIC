6a3bfd5a347e49a25c10311df01a259798452fce
ayun0614 <conpaku82@gmail.com>
2022-02-21 23:33:21 +0900

    네비게이션 드로어 추가

--- app/src/main/java/com/example/bbic/MainActivity.java
+++ app/src/main/java/com/example/bbic/MainActivity.java
@@ -1,15 +1,75 @@
 package com.example.bbic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //버튼 클릭 리스너 클래스
+    class BtnOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            switch (view.getId()){
+                //case를 통해 id에 따른 클릭이벤트 실행
+                case R.id.main_menu_ibtn:
+                    drawerLayout.openDrawer(drawerView);
+                    break;
+            }
+        }
+    }
+
+    //참조를 위한 각 객체 생성
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+    private ImageButton menuIbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //김동훈
+
+        //버튼 클릭 리스너 클래스 객체 생성(클릭 이벤트를 위함)
+        BtnOnClickListener onClickListener = new BtnOnClickListener();
+
+        //각 객체의 참조값을 넣어줌
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_activity);
+        drawerView = (View) findViewById(R.id.drawer_main);
+        menuIbtn = (ImageButton) findViewById(R.id.main_menu_ibtn);
+
+        //레이아웃에 네비게이션 드로어 설젇
+        drawerLayout.setDrawerListener(drawerListener);
+
+        //버튼의 클릭 리스너 설정
+        menuIbtn.setOnClickListener(onClickListener);
     }
+
+    //드로어 이벤트 리스너
+    DrawerLayout.DrawerListener drawerListener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
+
 }
