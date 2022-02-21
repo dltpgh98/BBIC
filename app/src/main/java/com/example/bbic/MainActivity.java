bfba2178db09319d47204ccceb62b51fb73f18ab
user <dltpgh98@gmail.com>
2022-02-21 23:25:42 +0900

    Create Viewpager2, SlidingUpPanel

--- app/src/main/java/com/example/bbic/MainActivity.java
+++ app/src/main/java/com/example/bbic/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.bbic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //이세호
+
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+        ViewpagerAdapter adapter = new ViewpagerAdapter(setTextList());
+        viewPager.setAdapter(adapter);
+
+
+    }
+    protected ArrayList setTextList(){
+
+        ArrayList<String> itemList = new ArrayList();
+        itemList.add("Page 1");
+        itemList.add("Page 2");
+        itemList.add("Page 3");
+        itemList.add("Page 4");
+        itemList.add("Page 5");
+
+        return itemList;
     }
 }
