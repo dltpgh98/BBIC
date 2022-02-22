868783a23bbccb2f39d33857e3d7245896b324a2
dltpgh98 <82332337+dltpgh98@users.noreply.github.com>
2022-02-22 22:39:48 +0900

    commit

---
+++ app/src/main/java/com/example/bbic/Login_Activity.java
@@ -0,0 +1,27 @@
+package com.example.bbic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        Button login_btn = (Button)findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Maps_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
