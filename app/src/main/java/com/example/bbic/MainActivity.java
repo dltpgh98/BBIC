96322671b2f06a65f5d2eb336cbdd50254ba5e6e
ayun0614 <conpaku82@gmail.com>
2022-02-23 00:16:49 +0900

    네비게이션 드로어 정보 추가
    웹 크롤링을 통한 날씨, 미세먼지, 온도 정보 추가

--- app/src/main/java/com/example/bbic/MainActivity.java
+++ app/src/main/java/com/example/bbic/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.bbic;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private View drawerView;
     private ImageButton menuIbtn;
+    private TextView temText, fineText, ultraText;
+
+
+    private final String temURL = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=경기도부천시날씨"; //웹크롤링 할 주소
+    private String allDust, weather, tem, fineDust, ultraFineDust;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +51,123 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //버튼 클릭 리스너 클래스 객체 생성(클릭 이벤트를 위함)
+
         BtnOnClickListener onClickListener = new BtnOnClickListener();
 
         //각 객체의 참조값을 넣어줌
         drawerLayout = (DrawerLayout) findViewById(R.id.main_activity);
         drawerView = (View) findViewById(R.id.drawer_main);
         menuIbtn = (ImageButton) findViewById(R.id.main_menu_ibtn);
+        temText = (TextView) findViewById(R.id.drawer_tem_text);
+        fineText = (TextView) findViewById(R.id.drawer_fine_text);
+        ultraText = (TextView) findViewById(R.id.drawer_ultra_text);
 
         //레이아웃에 네비게이션 드로어 설젇
         drawerLayout.setDrawerListener(drawerListener);
 
         //버튼의 클릭 리스너 설정
         menuIbtn.setOnClickListener(onClickListener);
+
+        //스레드간 데이터 전달을 위한 번들 생성
+        final Bundle bundle = new Bundle();
+
+        //웹크롤링 전용 스레드 생성
+        new Thread(){
+            @Override
+            public void run(){
+                Document doc ;
+                try {
+                    //웹 사이트 데이터 설정
+                    doc = Jsoup.connect(temURL).get();
+
+                    //온도정보
+                    Elements temperature = doc.select(".temperature_text");
+                    //미세먼지, 초미세먼지 정보
+                    Elements dust = doc.select(".report_card_wrap");
+                    //날씨
+                    Elements wt = doc.select(".weather_main");
+
+                    //문자열 변환 및 자르기
+                    tem = temperature.get(0).text().substring(5);
+                    allDust = dust.get(0).text();
+                    String[] array = allDust.split(" ");
+                    fineDust = array[1];
+                    ultraFineDust = array[3];
+                    weather = wt.get(0).text();
+
+                    //번들에 문자열 포장
+                    bundle.putString("temperature",tem);
+                    bundle.putString("fine",fineDust);
+                    bundle.putString("ultra",ultraFineDust);
+                    bundle.putString("weather",weather);
+                    Message msg = handler.obtainMessage();
+
+                    //메세지내용 번들로 지정
+                    msg.setData(bundle);
+
+                    //핸들러에 메세지 전달
+                    handler.sendMessage(msg);
+
+                    //10분마다 한번씩 실행
+                    Thread.sleep(600000);
+                } catch (IOException | InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            Bundle bundle = msg.getData();
+
+            drawer_input();
+        }
+    };
+
+    private void drawer_input()
+    {
+        temText.setText(tem+"C");
+
+        switch (fineDust)
+        {
+            case "좋음":
+                fineText.setTextColor(getResources().getColor(R.color.teal_200));
+                break;
+            case "보통":
+                fineText.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case "나쁨":
+                fineText.setTextColor(getResources().getColor(R.color.orange));
+                break;
+            case "매우나쁨":
+                fineText.setTextColor(getResources().getColor(R.color.red));
+                break;
+            default:
+                fineText.setTextColor(getResources().getColor(R.color.black));
+                break;
+        }
+
+        switch (ultraFineDust)
+        {
+            case "좋음":
+                ultraText.setTextColor(getResources().getColor(R.color.teal_200));
+                break;
+            case "보통":
+                ultraText.setTextColor(getResources().getColor(R.color.green));
+                break;
+            case "나쁨":
+                ultraText.setTextColor(getResources().getColor(R.color.orange));
+                break;
+            case "매우나쁨":
+                ultraText.setTextColor(getResources().getColor(R.color.red));
+                break;
+            default:
+                ultraText.setTextColor(getResources().getColor(R.color.black));
+                break;
+        }
     }
 
     //드로어 이벤트 리스너
