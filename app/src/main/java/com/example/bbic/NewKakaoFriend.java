29fe924e4d43daf9866a457bd2eaf21e489f3a5e
dltpgh98 <82332337+dltpgh98@users.noreply.github.com>
2022-06-23 07:02:56 +0900

    commit

--- app/src/main/java/com/example/bbic/NewKakaoFriend.java
+++ app/src/main/java/com/example/bbic/NewKakaoFriend.java
@@ -1,11 +1,22 @@
 package com.example.bbic;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bbic.Adapter.FriendAskAdapter;
+import com.example.bbic.Data.Friend;
+
+import java.util.List;
+
 public class NewKakaoFriend extends AppCompatActivity {
+
+    private ListView listView;
+    private FriendAskAdapter adapter;
+    private List<Friend> friendList;
+    private List<Friend> userFriendlist;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
