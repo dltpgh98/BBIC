76c55a73ed486b70de865fe841daec4c28993bb2
Seongdae Do <81352120+das03183@users.noreply.github.com>
2022-10-29 22:45:07 +0900

    do branch 길찾기 내역 이미지 및 고스트 기능 원활화 0.1.3

--- app/src/main/java/com/example/bbic/DB/UpdateGhostRequest.java
+++ app/src/main/java/com/example/bbic/DB/UpdateGhostRequest.java
@@ -12,13 +12,13 @@ public class UpdateGhostRequest extends StringRequest{
     final static private String URL = "http://ec2-13-124-60-158.ap-northeast-2.compute.amazonaws.com/updateghost.php";
     private Map<String, String> map;
 
-    public UpdateGhostRequest(long K_code, int K_ghost, Response.Listener<String> listener) {
+    public UpdateGhostRequest(long K_code, int K_ghost ,int P_location, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
         map.put("K_code",K_code +"");
         map.put("K_ghost", K_ghost + "");
-
+        map.put("P_location", P_location + "");
     }
 
     @Override
